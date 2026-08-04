4e6a694f94ace6506d1c40b0fef75e767bd49d05
Sai Ardhendu <saiardhendu10@gmail.com>
2026-08-04 23:12:07 +0530

    Time: 9 ms (41.38%), Space: 20.6 MB (21%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        s = s.lower()
+        l = 0
+        r = len(s) -1
+        while(l<r):
+            if s[l]!=s[r]:
+                return False
+            l+=1
+            r-=1
+        return True
+        
