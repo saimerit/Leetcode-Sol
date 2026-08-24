af4e1b723911c71e032847ef3d6c556f594c0f93
Sai Ardhendu <saiardhendu10@gmail.com>
2026-08-25 03:34:21 +0530

    Time: 110 ms (48.84%), Space: 95.1 MB (56.09%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+
+        vector<long> pref (n);
+        pref[0] = stones[0];
+
+        for(int i = 1; i < n; i++){
+            pref[i] = pref[i-1] + stones[i];
+        }  
+        long ans = pref[n-1];
+        for(int i = n-2; i >= 1; i--){
+            ans = max(ans, pref[i] - ans);
+        }
+        return (int) ans;
+    }
+};
