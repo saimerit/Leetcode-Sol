891be141c9237fa08cd86ad12597b35364dece17
Sai Ardhendu <saiardhendu10@gmail.com>
2026-08-28 01:13:03 +0530

    Time: 34 ms (84.17%), Space: 51.1 MB (83.56%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,75 @@
+class LRUCache {
+public:
+    class Node{
+        public:
+            int key, val;
+            Node* next;
+            Node* prev;
+
+            Node(int k, int v){
+                key = k;
+                val = v;
+                next = prev = NULL;
+            }
+    };
+    
+    
+    int limit;
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
+    unordered_map<int, Node*> m;
+
+    LRUCache(int capacity) {
+        limit = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if(m.find(key) == m.end()) return -1;
+        Node* a = m[key];
+        int ans = a->val;
+        m.erase(key);
+        delNode(a);
+        addNode(a);
+        m[key] = a;
+        return ans;
+    }
+    
+    void addNode(Node* nn){
+        Node* oldNext = head->next;
+        head->next = nn;
+        oldNext->prev = nn;
+        nn->next = oldNext;
+        nn->prev = head;
+    }
+
+    void delNode(Node* oldNode){
+        Node* oldp = oldNode->prev;
+        Node* oldn = oldNode->next;
+        oldp->next = oldn;
+        oldn->prev = oldp;
+    }
+
+    void put(int key, int value) {
+        if(m.find(key) != m.end()){
+            Node* oldNode = m[key];
+            delNode(oldNode);
+            m.erase(key);
+        }
+        if(m.size() == limit){
+            m.erase(tail->prev->key);
+            delNode(tail->prev);
+        }
+        Node* newNode = new Node(key, value);
+        addNode(newNode);
+        m[key] = newNode;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
