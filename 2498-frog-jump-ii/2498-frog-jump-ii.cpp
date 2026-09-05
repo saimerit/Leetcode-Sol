e727d33257847ab8192555036008c05e2e57b253
Sai Ardhendu <saiardhendu10@gmail.com>
2026-09-05 11:28:06 +0530

    Time: 3 ms (23.44%), Space: 69.2 MB (17.08%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -4,13 +4,8 @@ public:
         int n = stones.size();
         if(n == 1) return 0;
         if(n == 2) return abs(stones[1] - stones[0]);
-
-        vector<int> dp(n);
-        dp[0] = 0;
-        dp[1] = abs(stones[1] - stones[0]);
         int l = 0;
         for(int i = 2; i < n; i++){
-            dp[i] = min(dp[i-1] + abs(stones[i] - stones[i-1]), dp[i-2] + abs(stones[i] - stones[i-2]));
             l = max(l, max(abs(stones[i] - stones[i-1]), abs(stones[i] - stones[i-2])));
         }
         return l;
