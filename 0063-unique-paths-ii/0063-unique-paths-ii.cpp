ebd1a5d0537430ae88db3145b19ba8af00935515
Sai Ardhendu <saiardhendu10@gmail.com>
2026-09-07 10:02:29 +0530

    Time: 0 ms (100%), Space: 11.9 MB (31.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if(obstacleGrid[0][0] == 1) return 0;
+        
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, 0));
